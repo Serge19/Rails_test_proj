e7e2ed99882e57e362c7f130e8cab17c0801d4c9
Serge19 <sergey_zharkov@rambler.ru>
2017-06-13 09:11:33 +0300

    Edit file comments_controller.rb

--- ProjectRails/app/controllers/comments_controller.rb
+++ ProjectRails/app/controllers/comments_controller.rb
@@ -16,6 +16,7 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article)
   end
 
+
   private
     def comment_params
         params.require(:comment).permit(:commenter, :body)
